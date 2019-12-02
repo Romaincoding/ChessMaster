f7679ed9cb0cb76ac733254fac8aab26c079a7de
Romain <romain.dupuis@edu.itescia.fr>
2019-12-02 22:40:18 +0100

    ajout classe piece,attributs + getters, setters

---
+++ src/main/java/fr/rphstudio/chess/game/Piece.java
@@ -0,0 +1,85 @@
+package fr.rphstudio.chess.game;
+
+import fr.rphstudio.chess.interf.EmptyCellException;
+import fr.rphstudio.chess.interf.IChess;
+import fr.rphstudio.chess.interf.OutOfBoardException;
+
+import java.util.List;
+
+public class Piece implements IChess {
+
+    private static ChessColor color;
+    private static ChessType type;
+
+
+    public Piece (){
+
+    }
+
+    public static ChessColor getColor() {
+        return color;
+    }
+
+    public static void setColor(ChessColor color) {
+        Piece.color = color;
+    }
+
+    public static ChessType getType() {
+        return type;
+    }
+
+    public static void setType(ChessType type) {
+        Piece.type = type;
+    }
+
+
+    @Override
+    public void reinit() {
+
+    }
+
+    @Override
+    public ChessType getPieceType(ChessPosition p) throws EmptyCellException, OutOfBoardException {
+        return null;
+    }
+
+    @Override
+    public ChessColor getPieceColor(ChessPosition p) throws EmptyCellException, OutOfBoardException {
+        return null;
+    }
+
+    @Override
+    public int getNbRemainingPieces(ChessColor color) {
+        return 0;
+    }
+
+    @Override
+    public List<ChessPosition> getPieceMoves(ChessPosition p) {
+        return null;
+    }
+
+    @Override
+    public void movePiece(ChessPosition p0, ChessPosition p1) {
+
+    }
+
+    @Override
+    public ChessKingState getKingState(ChessColor color) {
+        return null;
+    }
+
+    @Override
+    public List<ChessType> getRemovedPieces(ChessColor color) {
+        return null;
+    }
+
+    @Override
+    public boolean undoLastMove() {
+        return false;
+    }
+
+    @Override
+    public long getPlayerDuration(ChessColor color, boolean isPlaying) {
+        return 0;
+    }
+}
