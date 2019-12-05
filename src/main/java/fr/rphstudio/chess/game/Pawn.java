fec01e749755754cb3617e023d414865e13f2aaf
Romain <romain.dupuis@edu.itescia.fr>
2019-12-05 22:45:48 +0100

    Pawn almost ok

--- src/main/java/fr/rphstudio/chess/game/Pawn.java
+++ src/main/java/fr/rphstudio/chess/game/Pawn.java
@@ -16,58 +16,26 @@ public class Pawn implements IMove {
      * @param rookMoves
      * @param varX
      * @param varY
-     * @param dist
+     * @param eatAllowed
      */
-    public static void addPawnMove(IChess.ChessPosition pos, Board board, List<IChess.ChessPosition> rookMoves,int varX, int varY,int dist) {
+    public static void addPawnMove(IChess.ChessPosition pos, Board board, List<IChess.ChessPosition> rookMoves,int varX, int varY,boolean eatAllowed) {
         IChess.ChessPosition posRef = new IChess.ChessPosition(pos.x, pos.y);
-        //List<IChess.ChessPosition> pawnMoves = new ArrayList<>();
-        for (int i = 0; i < dist; i++) {
-            posRef.x = posRef.x + varX;
-            posRef.y = posRef.y + varY;
-            IChess.ChessPosition validpos = new IChess.ChessPosition(posRef.x, posRef.y);
-            if (validpos.x >= 0 && validpos.x <= 7 && validpos.y >= 0 && validpos.y <= 7) {
-                if (board.getPiece(validpos) == null) {
-                    rookMoves.add(validpos);
-                }
-                else {
-                    /*Piece myPawn = board.getPiece(pos);
-                    IChess.ChessColor colorofPawn = myPawn.getColor();
-                    Piece otherPiece = board.getPiece(validpos);
-                    IChess.ChessColor colorOtherPiece = otherPiece.getColor();
-                    posRef.x = posRef.x + 1;
-                    posRef.y = posRef.y - 1;
-                    IChess.ChessPosition validpos1 = new IChess.ChessPosition(posRef.x, posRef.y);
-                        if (board.getPiece(validpos1) != null && colorOtherPiece != colorofPawn) {
-                            System.out.println("ca marche");
-                            rookMoves.add(validpos);
-
-
-
-                        }*/
-                    /*
-
-                    if (colorofRook != colorOtherPiece) {
-                        rookMoves.add(validpos);
-                    }
-
-                     */
-                        break;
-                    }
+        
+        IChess.ChessPosition posDest = new IChess.ChessPosition(pos.x + varX, pos.y + varY);
+        Piece piece = board.getPiece(posDest);
+        Piece mypiece = board.getPiece(pos);
+        if (piece != null) {
+            if (eatAllowed == true) {
+                if (piece.getColor() != mypiece.getColor()) {
+                    rookMoves.add(posDest);
                 }
             }
+        } else {
+            if (eatAllowed == false) {
+                rookMoves.add(posDest);
+            }
         }
-
-    /*
-    public List<IChess.ChessPosition> getPossibleMoves(IChess.ChessPosition pos, Board board) {
-        System.out.println("test 1");
-        List<IChess.ChessPosition> pawnMoves = new ArrayList<>();
-        ChessUtility.addXYMove(pos,board,pawnMoves,0, 0, IChess.ChessColor.CLR_BLACK);
-        ChessUtility.addXYMove(pos,board,pawnMoves,0, 0, IChess.ChessColor.CLR_WHITE);
-
-        return pawnMoves;
     }
-
-     */
 
 
     /**
@@ -88,18 +56,31 @@ public class Pawn implements IMove {
             IChess.ChessPosition posRef = new IChess.ChessPosition(pos.x  ,pos.y);
             IChess.ChessPosition validpos = new IChess.ChessPosition(posRef.x ,posRef.y);
 
+            /* Avancer */
             if(pos.y == 6){
-                Pawn.addPawnMove(pos, board, pawnMoves, 0, -1,2);
+                Pawn.addPawnMove(pos, board, pawnMoves, 0, -2,false);
+
+            }
+            else {
+                Pawn.addPawnMove(pos, board, pawnMoves, 0, -1,false);
             }
 
-            else { Pawn.addPawnMove(pos, board, pawnMoves, 0, -1,1); }
+            /* Manger */
+            Pawn.addPawnMove(pos, board, pawnMoves, -1, -1,true);
+            Pawn.addPawnMove(pos, board, pawnMoves, 1, -1,true);
         }
         else {
             if(pos.y == 1){
-                Pawn.addPawnMove(pos, board, pawnMoves, 0, 1,2);
+                Pawn.addPawnMove(pos, board, pawnMoves, 0, 2,false);
             }
-            else {Pawn.addPawnMove(pos,board,pawnMoves,0, 1, 1);}
+            else {
+                Pawn.addPawnMove(pos,board,pawnMoves,0, 1,false);
+            }
+            /* Manger */
+            Pawn.addPawnMove(pos, board, pawnMoves, -1, 1,true);
+            Pawn.addPawnMove(pos, board, pawnMoves, 1, 1,true);
         }
+
 
         //ChessUtility.addXYMove(pos,board,pawnMoves,1,-1,1);
         //ChessUtility.addXYMove(pos,board,pawnMoves,-1,-1,1);
